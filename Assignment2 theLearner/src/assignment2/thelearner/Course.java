af15edf6e2c775fe534210e3d7cba2858d7ff658
deepsharma29 <deepalisharma1256@yahoo.com>
2017-01-25 11:29:32 -0500

    "Course class created"

---
+++ Assignment2 theLearner/src/assignment2/thelearner/Course.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2.thelearner;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author c0689321
+ */
+public class Course {
+
+    List<Student> students;
+
+    public void Course() {
+
+    }
+
+    public void Course(List<Student> students) {
+
+    }
+
+    public void add(Student student) {
+
+    }
+
+    public void remove(String id) {
+
+    }
+
+    public void remove(int position) {
+
+    }
+
+    public void insert(Student student, int position) {
+
+    }
+
+    public Student get(String id) {
+        return null;
+    }
+
+    public Student get(int position) {
+        return null;
+    }
+
+    public List<Student> getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public Set<Student> getAllGender(String gender) {
+        return null;
+    }
+
+    public Map<String, Set<Student>> getGradeMap() {
+        return null;
+    }
+}
