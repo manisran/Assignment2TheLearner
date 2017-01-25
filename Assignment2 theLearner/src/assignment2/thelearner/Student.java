36a30a71ff284ffefecfed8e777c8716ffd934b1
kamaljit <c0687969@mylambton.ca>
2017-01-25 11:02:55 -0500

    "Created Student Class"

---
+++ Assignment2 theLearner/src/assignment2/thelearner/Student.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2.thelearner;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author c0687969
+ */
+public class Student {
+       String name;
+    String id;
+    String gender;
+    double grade;
+    
+    public Student(){
+        
+    }
+     public Student(String name, String id, String gender, double grade) {
+        this.name = name;
+        this.id = id;
+        this.gender = gender;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (Double.doubleToLongBits(this.grade) != Double.doubleToLongBits(other.grade)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.gender, other.gender)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", id=" + id + ", gender=" + gender + ", grade=" + grade + '}';
+    }
+     
+     
+}
+    
+
